2a7d0cbb9e5765af7f8dd7a2d38af4088d59b501
ayushichouhan04 <ayushi@complitech.net>
2023-08-16 18:23:27 +0530

    Program 12 is done

---
+++ program12.rb
@@ -0,0 +1,8 @@
+#Find an area of parallelogram
+length = gets.chomp.to_i;
+altitude = gets.chomp.to_i;
+
+#calculate area of a parallelogram
+area = length * altitude;
+
+puts "Area of the parallelogram is #{area}"
