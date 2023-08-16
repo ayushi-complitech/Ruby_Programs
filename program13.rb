213968b6a30634c66f8da48e2b75487b30646507
ayushichouhan04 <ayushi@complitech.net>
2023-08-16 18:31:27 +0530

    Done

--- program13.rb
+++ program13.rb
@@ -1,4 +1,4 @@
-#Calculate are of cube
+#Calculate area of cube
 side = gets.chomp.to_i #Enter edge of a cube
 
 area = 6 * (side ** 2)
---
+++ program14.rb
@@ -0,0 +1,6 @@
+#Calculate volume of cube
+side = gets.chomp.to_i #Enter edge of a cube
+
+volume = side ** 3
+
+puts volume
