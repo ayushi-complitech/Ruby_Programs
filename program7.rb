ed77fd7a8af6ca390fdaed77ef4784738deb3f6f
ayushichouhan04 <ayushi@complitech.net>
2023-08-16 17:08:25 +0530

    Task done

---
+++ program7.rb
@@ -0,0 +1,13 @@
+#Check a number is prime or not
+puts "Enter a number:"
+num = gets.chomp.to_i
+
+def is_prime(num)
+  (2..(num - 1)).each do |n|
+    return "#{num} is not a prime number" if num % n == 0
+  end
+  return "#{num} is a prime number"
+end
+
+a = is_prime(num)
+puts a
