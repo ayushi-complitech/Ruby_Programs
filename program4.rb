b0b81f246f7ce09b989f68520d2922c3ea5e3ee0
ayushichouhan04 <ayushi@complitech.net>
2023-08-16 12:52:26 +0530

    Program

--- program4.rb
+++ program4.rb
@@ -5,11 +5,7 @@ base=gets.chomp.to_i
 puts "Enter exponent:-"
 exponent=gets.chomp.to_i
 
-y = 1
-
-for i in 1..exponent
-  y = base * y #update value of y.
-end
+result = base ** exponent
 
 #result is here
-puts "The result is #{y}"
+puts "The result is #{result}"
