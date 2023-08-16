eab16c4e5892ddbeeeb3502d6607b9caf04a3a38
ayushichouhan04 <ayushi@complitech.net>
2023-08-16 12:02:00 +0530

    Program 5

--- program5.rb
+++ program5.rb
@@ -1,10 +1,10 @@
-first_num = 0
-second_num = 1
+first_num = gets.chomp.to_i
+second_num = gets.chomp.to_i
 
 puts first_num
 puts second_num
 
-while second_num < 10
+while second_num < 100
   num = first_num + second_num
   puts num
   first_num = second_num
