761423a86b84d61c7b1b1d1827b87e784f1cd20c
ayushichouhan04 <ayushi@complitech.net>
2023-08-16 17:37:27 +0530

    Program done

--- program8.rb
+++ program8.rb
@@ -1,8 +1,9 @@
+#calculate the sum of all even numbers
 array = [1,2,3,4,5,6,7,8,9,10]
 sum = 0
 
 array.each do |total|
-  if total % 2 == 0
+  if total % 2 == 0 #find number is even or not
    sum += total
   end
 end
--- program9.rb
+++ program9.rb
@@ -1,8 +1,9 @@
+#calculate the sum of all odd numbers
 array = [1,2,3,4,5,6,7,8,9,10]
 sum = 0
 
 array.each do |total|
-  if total % 2 == 0
+  if total % 2 == 0 #check number is even or odd
   else
     sum += total
   end
