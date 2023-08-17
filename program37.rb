0f7f175d0940d7ef7b8e929a755c45c1a5ad2bd7
ayushichouhan04 <ayushi@complitech.net>
2023-08-17 18:32:20 +0530

    Array program

---
+++ program37.rb
@@ -0,0 +1,6 @@
+#find largest and smallest element
+array = [1,2,3,4,5,6,7,8,9]
+max = array.max
+min = array.min
+puts "Largest number in array is : #{max}"
+puts "Smallest number in array is : #{min}"
