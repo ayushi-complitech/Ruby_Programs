19fbd6ad68473f21b8c17b8e766e1d7c825fc142
ayushichouhan04 <ayushi@complitech.net>
2023-08-17 17:47:35 +0530

    Array program

--- program30.rb
+++ program30.rb
@@ -2,6 +2,7 @@
 
 array1 = [1,2,3,4,5,6]
 array2 = [4,5,6,7,8,9]
-array3 = (array1 + array2).uniq
+array3 = (array1 + array2).uniq #array1 | array2
 
 print "#{array3} \n "
+
---
+++ program32.rb
@@ -0,0 +1,6 @@
+#find uncommon element from two array
+array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+array2 = [3, 4, 5]
+new_array = array1 - array2
+
+print "#{new_array} \n"
