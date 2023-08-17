2e87abb385e924277e6409eb3474b3a9e8c280b5
ayushichouhan04 <ayushi@complitech.net>
2023-08-17 11:20:45 +0530

    Ternary operator use

--- program16.rb
+++ program16.rb
@@ -1,5 +1,4 @@
 #Check number is even or odd
 num = gets.chomp.to_i
 
-if (num % 2 == 0) ? (puts "#{num} is even") : (puts "#{num} is odd");
-end
+(num % 2 == 0) ? (puts "#{num} is even") : (puts "#{num} is odd");
