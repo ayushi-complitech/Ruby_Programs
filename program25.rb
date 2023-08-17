3657d0081ce0e7d66bf5de97cb5ab1c2e47c4422
ayushichouhan04 <ayushi@complitech.net>
2023-08-17 13:52:20 +0530

    Program 25 Done

---
+++ program25.rb
@@ -0,0 +1,8 @@
+dividend = gets.chomp.to_i
+divisor = gets.chomp.to_i
+
+quotient = dividend / divisor
+remainder = dividend % divisor
+
+puts "The quotient of #{dividend} and #{divisor} is #{quotient}"
+puts "The remainder of #{dividend} and #{divisor} is #{remainder}"
