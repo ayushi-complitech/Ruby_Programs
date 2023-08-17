099d00159aaf8279f36459620e1ae4e9ff56834b
ayushichouhan04 <ayushi@complitech.net>
2023-08-17 15:50:12 +0530

    Program 27 done

--- program27.rb
+++ program27.rb
@@ -1,11 +1,11 @@
 #call a global variable inside method
 $global_variable = 10
 
-class Customer
-   def print_global
+class Customer #create a class
+   def print_global # create a method
       puts "Global variable in customer is #$global_variable."
    end
 end
 
-object = Customer.new
+object = Customer.new # object creation
 object.print_global
