fa5d11e357ef4f1f72bdc142c7efb4b6c8aae1df
ayushichouhan04 <ayushi@complitech.net>
2023-08-21 13:03:26 +0530

    Hash program

---
+++ program47.rb
@@ -0,0 +1,9 @@
+#hash program
+
+people = {"1" => "Person1", "2" => "Person2", "3" => "Person3"}
+
+keys = people.keys
+values = people["1"],people["2"],people["3"]
+
+puts "keys = #{keys}"
+puts "values = #{values}"
