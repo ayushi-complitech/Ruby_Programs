5a48fa2ce896488836217f400971531f5bff9e33
ayushichouhan04 <ayushi@complitech.net>
2023-08-22 12:52:26 +0530

    Percentage

--- program53.rb
+++ program53.rb
@@ -7,8 +7,7 @@ print "Enter second number : "
 num2 = gets.chomp.to_i
 
 diff = (num1 > num2) ? (num1 - num2) : (num2 - num1)
-avg = (num1 + num2) / 2
 
-percentage = (diff * 100) / avg
+percentage = (diff * 100) / num1
 
 puts "percentage between two numbers is #{percentage}"
