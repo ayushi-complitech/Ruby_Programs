c742e36edfabfeee46ad2ac9a535738ca1a12b5f
ayushichouhan04 <ayushi@complitech.net>
2023-08-28 11:54:23 +0530

    sum of 2 integer

--- program1.rb
+++ program1.rb
@@ -1,9 +1,22 @@
-#take input from user
-a = gets.chomp.to_i;
-b = gets.chomp.to_i;
+# take an input from user
+print "Enter the first integer: "
+num1 = gets.chomp
 
-#get sum of inputs
-c = a + b
+print "Enter the second integer: "
+num2 = gets.chomp
 
-#result
-puts "sum of a and b is #{c}";
+begin
+  # Convert the input to integers
+  num1 = Integer(num1)
+  num2 = Integer(num2)
+
+  # Perform the addition operation
+  sum = num1 + num2
+
+  # Display the result
+  puts "The sum of #{num1} and #{num2} is #{sum}."
+rescue ArgumentError
+  # Handle the exception if the user enters non-integer values
+  puts "Invalid input. Please enter integers only."
+end
+
