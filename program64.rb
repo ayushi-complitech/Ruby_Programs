59344a4041ec7286be7209ef0ddc968af752d0f0
ayushichouhan04 <ayushi@complitech.net>
2023-09-01 16:51:10 +0530

    Single inheritance

---
+++ program64.rb
@@ -0,0 +1,26 @@
+#single inheritance
+#Student class
+class Student
+
+  #constructor method of super class
+  def initialize
+  	puts "This is student class."
+  end
+  
+  #method of super class
+  def name
+    puts "I am Ayushi."
+  end
+end
+
+class Marks < Student
+  #constructor method of sub class
+  def initialize
+  	puts "Ayushi's marks is 70."
+  end
+end
+
+Student.new
+info = Marks.new
+
+info.name
---
+++ program65.rb
@@ -0,0 +1,25 @@
+#single inheritance
+#Student class
+class Student
+
+  #constructor method of super class
+  def initialize
+  	puts "This is student class."
+  end
+  
+  #method of super class
+  def name
+    puts "I am Ayushi."
+  end
+end
+
+class Marks < Student
+  #method override
+  def name
+  	puts "Hello Ayushi!!"
+  end
+end
+
+info = Marks.new
+
+info.name
