072dad70818c32d20d48a3082aa8bad2b2aeba94
ayushichouhan04 <ayushi@complitech.net>
2023-09-01 17:46:54 +0530

    Overidden method

---
+++ program66.rb
@@ -0,0 +1,30 @@
+#single inheritance with overidden method
+#Student class
+class Student
+
+  #constructor method of super class
+  def initialize
+  	puts "This is student class."
+  end
+  
+  #method of super class
+  def name
+    puts "I am Ayushi."
+  end
+end
+
+class Marks < Student
+  #method override
+  def initialize
+    puts "Hello student!"
+  end
+
+  def name
+    super()
+  	puts "Hello Ayushi!!"
+  end
+end
+
+info = Marks.new
+
+info.name
