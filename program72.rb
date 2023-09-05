fa412e6e5fecfd7dcb7791c0ce321908721a7b41
ayushichouhan04 <ayushi@complitech.net>
2023-09-05 13:03:00 +0530

    API

--- program72.rb
+++ program72.rb
@@ -1,24 +1,10 @@
-#API program
-
+#API Program
 require 'net/http'
+require 'json'
 
-print "ip address : "
-puts Net::HTTP.get(URI('https://ipapi.co/ip/'))
-print "city : "
-puts Net::HTTP.get(URI('https://ipapi.co/city/'))
-print "region : "
-puts Net::HTTP.get(URI('https://ipapi.co/region/'))
-print "country : "
-puts Net::HTTP.get(URI('https://ipapi.co/country/'))
-print "continent : "
-puts Net::HTTP.get(URI('https://ipapi.co/continent_code/'))
-print "postal code : "
-puts Net::HTTP.get(URI('https://ipapi.co/postal/'))
-print "latitude : "
-puts Net::HTTP.get(URI('https://ipapi.co/latitude/'))
-print "longitude : "
-puts Net::HTTP.get(URI('https://ipapi.co/longitude/'))
-print "timezone : "
-puts Net::HTTP.get(URI('https://ipapi.co/timezone/'))
-print "currency : "
-puts Net::HTTP.get(URI('https://ipapi.co/currency/'))
+loc = Net::HTTP.get(URI("https://ipapi.co/json/"))
+show = JSON.parse(loc)
+
+show.each do |key,value|
+puts "#{key} => #{value}"
+end
